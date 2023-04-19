d3d23a949f3d26a9c1f8ab506a4276152024d8a0
ohdamnitsnikki <nicole_stjernberg@hotmail.com>
2023-04-19 19:37:31 +0000

    activate env key

--- secret_chef/settings.py
+++ secret_chef/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-16d%gc!mmucb!)dzff0#1^la751opj*d5q((h7uu0bt+hz_fov'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
