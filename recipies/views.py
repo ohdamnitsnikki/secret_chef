6280c6747b63bbafa193c7bd340c836e56e1efe7
ohdamnitsnikki <nicole_stjernberg@hotmail.com>
2023-04-21 21:59:52 +0000

    remove problems

--- recipies/views.py
+++ recipies/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
-from django.views import genericfrom 
-from .models import Post 
+from django.views import genericfrom
+from .models import Post
 
 
 # Display posts for users
 class PostList(generic.ListViews):
-    model = Post 
+    model = Post
     queryset = Post.objects.filter(status=1).order_by('-created_on')
     template_name = 'index.html'
     paginate_by = 6
