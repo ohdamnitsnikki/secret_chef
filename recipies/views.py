07cf7d41673eaa9b1873c4f1530bbd40d2efeac9
ohdamnitsnikki <nicole_stjernberg@hotmail.com>
2023-04-21 21:31:11 +0000

    make class to tell how posts will be displayed for user

--- recipies/views.py
+++ recipies/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 from django.views import genericfrom 
 from .models import Post 
+
+
+# Display posts for users
+class PostList(generic.ListViews):
+    model = Post 
+    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    template_name = 'index.html'
+    paginate_by = 6
