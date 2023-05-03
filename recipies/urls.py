5749f91cb8b3941a7a1ca3d27f1da5042778b1e0
ohdamnitsnikki <nicole_stjernberg@hotmail.com>
2023-05-03 10:40:46 +0000

    add paths to navlinks

--- recipies/urls.py
+++ recipies/urls.py
@@ -3,4 +3,7 @@ from django.urls import path
 
 urlpatterns = [
     path("", views.PostList.as_view(), name="home"),
+    path("", views.PostList.as_view(), name="about"),
+    path("", views.PostList.as_view(), name="post"),
+    path("", views.PostList.as_view(), name="login/signup"),
 ]
