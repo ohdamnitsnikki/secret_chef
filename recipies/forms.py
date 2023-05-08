64ab791769b97c856f0831f926204d09c6bf9c93
ohdamnitsnikki <nicole_stjernberg@hotmail.com>
2023-05-08 12:12:49 +0000

    add to RecipeForm class

--- recipies/forms.py
+++ recipies/forms.py
@@ -9,7 +9,26 @@ class CommentForm(forms.ModelForm):
         fields = ('body',)
 
 
-class RecipeForm(forms.ModelForm):
-    class Meta:
-        model = Recipe
-        fields = ['name', 'description', 'category', 'photo']
+class RecipeForm(forms.Form):
+    name = forms.CharField(
+        max_length=100,
+        widget=forms.TextInput(attrs={'class': 'form-control form-control-lg', 'placeholder': 'Name of dish'}),
+    )
+    description = forms.CharField(
+        widget=forms.Textarea(attrs={'class': 'form-control is-invalid', 'placeholder': 'Please provide us with a clear description and measurements of your recipe.', 'required': True}),
+        error_messages={'required': 'Please provide a description for your recipe.'},
+    )
+    category = forms.ChoiceField(
+        choices=[
+            ('', 'Choose category'),
+            ('breakfast', 'Breakfast'),
+            ('dinner', 'Dinner'),
+            ('dessert', 'Dessert'),
+        ],
+        widget=forms.Select(attrs={'class': 'custom-select', 'required': True}),
+        error_messages={'required': 'Please choose a category for your recipe.'},
+    )
+    photo = forms.ImageField(
+        widget=forms.FileInput(attrs={'class': 'custom-file-input', 'id': 'validatedCustomFile', 'required': True}),
+        error_messages={'required': 'Please upload a photo of your dish.'},
+    )
