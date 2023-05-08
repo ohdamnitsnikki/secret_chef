1143a076b40b755b82c0d41a348117efe9f0891b
ohdamnitsnikki <nicole_stjernberg@hotmail.com>
2023-05-08 10:28:54 +0000

    change submit_recipe path

--- recipies/urls.py
+++ recipies/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path("login/", views.login, name="login"),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
     path('like/<slug:slug>/', views.PostLike.as_view(), name="post_like"),
-    path('submit_recipe/', views.submit_recipe, name='submit_recipe'),
+    path('submit_recipe/', views.submit_recipe, {'form': RecipeForm}, name='submit_recipe'),
 ]
